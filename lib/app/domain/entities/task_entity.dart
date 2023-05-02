ddf42151cee95e7ad9ffb1873ae5357b628d4d74
vini.rod <vinicioswork@gmail.com>
2023-05-01 22:05:47 -0300

    improve task entity

--- lib/app/domain/entities/task_entity.dart
+++ lib/app/domain/entities/task_entity.dart
@@ -1,3 +1,4 @@
+import 'package:equatable/equatable.dart';
 import 'package:isar/isar.dart';
 import 'package:todo_list_app/app/domain/entities/task_status.dart';
 
@@ -5,11 +6,11 @@ part 'task_entity.g.dart';
 
 @Collection()
 class TaskEntity {
-  Id? id = Isar.autoIncrement;
-  String title;
+  Id? id;
+  final String title;
 
   @enumerated
-  TaskStatus status = TaskStatus.pending;
+  TaskStatus status;
 
   TaskEntity({this.id, required this.title, required this.status});
 }
